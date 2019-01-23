aeb121ddc97eb2b8f8b56da65983c3660b1bc439
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2019-01-23 21:44:34 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 
 require_relative '../../config/environment'
-
+require 'pry'
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -10,4 +10,46 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+  
+  get '/articles/new' do
+    
+    erb :new 
+  end 
+  
+  post '/articles' do 
+  
+    @article1 = Article.create(params)
+    
+     redirect to "/articles/#{@article1.id}"
+     
+  end 
+  
+  get '/articles/:id' do 
+    @article = Article.find_by(id: params[:id])
+    erb :show 
+  end 
+  
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+  
+  get '/articles/:id/edit' do 
+    @article2 = Article.find_by(id: params[:id])
+    erb :edit 
+  end 
+  
+  patch '/articles/:id' do 
+    @article3 = Article.find_by(id: params[:id])
+    @article3.title = params[:title]
+    @article3.content = params[:content]
+    @article3.save
+    erb :show
+  end 
+  
+  delete '/articles/:id' do
+    @article4 = Article.find_by(id: params[:id])
+    @article4.delete 
+  end
+  
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,15 @@
-#Placeholder for a model
 
 class Article < ActiveRecord::Base
+  
+#  attr_accessor :title, :content 
+#  @@all = []
+#  def initialize(params)
+    
+#    @title = params[:title]
+#    @content = params[:content]
+#    @@all << self 
+    
+#  end 
+  
+  
 end
