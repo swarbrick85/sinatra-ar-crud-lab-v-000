031a8cfa585bee602a67455fc987c90d600e99c7
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2019-01-23 19:24:53 +0000

    Automatically backed up by Learn

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,14 @@
 
 class Article < ActiveRecord::Base
   
-  attr_accessor :title
+  attr_accessor :title, :content 
+  @@all = []
+  def initialize(params)
+    
+    @title = params[:title]
+    @content = params[:content]
+    @@all << self 
+  end 
+  
   
 end
